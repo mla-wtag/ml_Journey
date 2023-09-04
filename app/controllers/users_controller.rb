7486e73e18f9e35b6a4e94c3e9422fdd7ad98855
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-04 13:49:09 +0200

    Show created user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:firstname, :lastname, :employee_id, :date_of_birth, :joining_day, :designation, :profile_picture, :email)
   end
