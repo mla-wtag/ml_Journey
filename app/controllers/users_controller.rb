923fedb87f22fc10c8509f60d71fbbc2ab1151d9
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-04 14:16:44 +0200

    update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   def user_params
     params.require(:user).permit(:firstname, :lastname, :employee_id, :date_of_birth, :joining_day, :designation, :profile_picture, :email)
   end
