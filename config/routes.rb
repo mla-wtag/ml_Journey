09e456a4974b3dc137cb3a4126ba8df29f230e3f
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-04 11:13:54 +0200

    index html site for user with abilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Rails.application.routes.draw do
+  root "users#index"
+  resources :users
+  # resources :journals do
+  # 	member do
+  # 		get :delete # adding the delete action manual, because rails doesn't give it to you.
+  # 	end
+  # end
+
 end
