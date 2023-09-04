4946bb6f2d3f8250b4d48eafdacd043dd909c140
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-04 14:34:25 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users
-  # resources :journals do
-  # 	member do
-  # 		get :delete # adding the delete action manual, because rails doesn't give it to you.
-  # 	end
-  # end
-
+  resources :users do
+    member do
+      get :delete # adding the delete action manual, because rails doesn't give it to you.
+    end
+  end
 end
