5b14c70f1f65dcd14728d00f70303e2f0ac586a8
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-04 11:58:56 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
