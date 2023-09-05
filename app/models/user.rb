dd7462f4e11486016d861f970b00b121cca10911
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-05 16:41:23 +0200

    Update usercode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
   validates :firstname, presence: true, uniqueness: true
-
-  # enum role: { user: 0, admin: 1 }
-
-  # after_initialize :set_default_role, :if => :new_record?
-
-  # def set_default_role
-  #   self.role ||= :user
-  # end
 end
