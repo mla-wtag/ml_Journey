ec94ca69bb82ee227a74f77184607ac81814a49e
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-09-05 17:00:27 +0200

    Validations Update for create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   validates :firstname, presence: true, uniqueness: true
+  validates :lastname, presence: true
+  validates :employee_id, presence: true
+  validates :date_of_birth, presence: true
+  validates :joining_day, presence: true
+  validates :designation, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
