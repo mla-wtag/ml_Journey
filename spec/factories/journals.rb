1a9bbfac59dcb6bbd00475685e2871b31654aefb
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-10-03 13:58:15 +0200

    Rspec tests

---
+++ spec/factories/journals.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :journal do
+    title { FFaker::Book.title }
+    date { FFaker::Time.between(1.years.ago, Date.today).to_date }
+    content { FFaker::Lorem.paragraph }
+    goals_today { FFaker::Lorem.sentence }
+    goals_tomorrow { FFaker::Lorem.sentence }
+    association :user
+  end
+end
--- spec/models/journals_spec.rb
+++ spec/models/journals_spec.rb
@@ -1,29 +1,47 @@
 require 'rails_helper'
 RSpec.describe Journal, type: :model do
-  let(:user1) { FactoryBot.create(:user) }
-  let(:valid_journal) { FactoryBot.create(:journal) }
+  let(:journal) { FactoryBot.create(:journal) }
 
   it 'checks if Journal can be created with all variables' do
-    except(user1).to be_valid
+    expect(journal).to be_valid
   end
 
   it 'validates presence of attributes' do
-    should validate_presence_of(:title)
-    should validate_presence_of(:date)
-    should validate_presence_of(:content)
-    should validate_presence_of(:goals_today)
-    should validate_presence_of(:goals_tomorrow)
+    should validate_presence_of(:title).with_message(I18n.t('validations.presence'))
+    should validate_presence_of(:date).with_message(I18n.t('validations.presence'))
+    should validate_presence_of(:content).with_message(I18n.t('validations.presence'))
+    should validate_presence_of(:goals_today).with_message(I18n.t('validations.presence'))
+    should validate_presence_of(:goals_tomorrow).with_message(I18n.t('validations.presence'))
   end
 
-  it 'checks that a journal with specific attributes is not valid' do
-    invalid_journal = FactoryBot.create(
-      :journal,
-      title: 'Name',
-      date: '2023-12-12',
-      content: 'A lot of content',
-      goals_today: 'A lot of goals',
-      goals_tomorrow: '',
-    )
+  it 'checks that a Journal without a title is not valid' do
+    invalid_journal = FactoryBot.build(:journal, title: nil)
     expect(invalid_journal).not_to be_valid
+  end
+
+  it 'checks that a Journal without a date is not valid' do
+    invalid_journal = FactoryBot.build(:journal, date: nil)
+    expect(invalid_journal).not_to be_valid
+  end
+
+  it 'checks that a Journal without content is not valid' do
+    invalid_journal = FactoryBot.build(:journal, content: nil)
+    expect(invalid_journal).not_to be_valid
+  end
+
+  it 'checks that a Journal without goals for today is not valid' do
+    invalid_journal = FactoryBot.build(:journal, goals_today: nil)
+    expect(invalid_journal).not_to be_valid
+  end
+
+  it 'checks that a Journal without goals for tomorrow is not valid' do
+    invalid_journal = FactoryBot.build(:journal, goals_tomorrow: nil)
+    expect(invalid_journal).not_to be_valid
+  end
+
+  describe 'Journal association with user' do
+    it 'belongs to a user' do
+      should belong_to(:user)
+    end
   end
 end
