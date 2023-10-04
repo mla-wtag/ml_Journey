5a5d90ec8082a6246910e9e5b93ac3ca8c305495
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-10-04 14:39:54 +0200

    link fix with all users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :authenticate_user
 
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to user_path(current_user)
   end
 end
