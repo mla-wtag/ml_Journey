6e1132fd1a13cebe437a0f3db23a752f419e4cb7
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-10-18 13:44:46 +0200

    validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,9 @@ class Task < ApplicationRecord
   has_many :users, through: :assignments
 
   enum status: { todo: 0, progress: 1, done: 2 }
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :date, presence: true
+  validates :description, presence: true, length: { maximum: 2500 }
+  validates :user_ids, presence: true
 end
