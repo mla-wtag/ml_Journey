89717a34e1754aad8b56378a29c87d07a2c6c800
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-11-14 23:00:29 +0100

    scss fix and some small fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,10 +32,10 @@ class UsersController < ApplicationController
   def update_role
     if @user.user_role?
       @user.update(role: 'admin_role')
-      flash[:alert] = "#{@user.first_name}" + t('alerts.ability_admin_role')
+      flash[:alert] = "#{@user.first_name} " + t('alerts.ability_admin_role')
     else @user.admin_role?
       @user.update(role: 'user_role')
-      flash[:alert] = "#{@user.first_name}" + t('alerts.ability_user_role')     end
+      flash[:alert] = "#{@user.first_name} " + t('alerts.ability_user_role')     end
     redirect_to users_path
   end
 
