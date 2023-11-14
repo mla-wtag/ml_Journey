703036b4d6c1f2bfd0fdff23773e270fbc9e1cd9
Michael Lavelanet <michael.lavelanet@welldev.io>
2023-11-14 12:03:29 +0100

    half finished design

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,6 +4,6 @@ class Goal < ApplicationRecord
   enum status: { todo: 0, progress: 1, done: 2 }
 
   validates :title, presence: true, length: { maximum: 255 }
-  validates :description, presence: true, length: { maximum: 5000 }
+  validates :description, presence: true, length: { maximum: 2500 }
   validates :date, presence: true
 end
